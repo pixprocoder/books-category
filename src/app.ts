7a034e381c1065c5f549cc1c8ccd1eb0dc73ca9b
pixprocoder <makkobir79@gmail.com>
2023-09-03 16:45:40 +0300

    ts-node dev added

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import cors from "cors";
+import express, { Application } from "express";
+import routers from "./app/routes";
+
+const app: Application = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/v1", routers);
+
+export default app;
