6656a83bfa57cc84077476e1a9b0ed4d94145fdc
pixprocoder <makkobir79@gmail.com>
2023-09-10 17:47:49 +0300

    root route set

--- src/app.ts
+++ src/app.ts
@@ -10,5 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/", (req, res) => {
+  res.send("Hello From Assignment 8");
+});
 app.use("/api/v1", routers);
 export default app;
